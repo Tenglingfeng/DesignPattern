c1086a8e43f500aab8ca16f7720b2623e8d13ed2
Tenglingfeng <704279465@qq.com>
2020-04-30 00:15:34 +0800

    123

---
+++ EFCore.Data/Migrations/20200429155418_Initial2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Data.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Population",
+                table: "Provinces",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AreaCode",
+                table: "Cities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Population",
+                table: "Provinces");
+
+            migrationBuilder.DropColumn(
+                name: "AreaCode",
+                table: "Cities");
+        }
+    }
+}
---
+++ EFCore.Data/MyDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using EFCore.DomainModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Province> Provinces { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Mayor> Mayors { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EFCoreDB;Trusted_Connection=True;");
+        //}
+
+        public DbSet<CityCompany> CityCompanies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CityCompany>().HasKey(x => new { x.CityId, x.CompanyId });
+            modelBuilder.Entity<CityCompany>().HasOne(x => x.City).WithMany(x => x.CityCompanies)
+                .HasForeignKey(x => x.CityId);
+            modelBuilder.Entity<CityCompany>().HasOne(x => x.Company).WithMany(x => x.CityCompanies)
+                .HasForeignKey(x => x.CompanyId);
+            modelBuilder.Entity<Mayor>().HasOne(x => x.City).WithOne(x => x.Mayor).HasForeignKey<Mayor>(x => x.CityId);
+        }
+    }
+}
